6b6f87068d47e865ca17e628db24621d0f9ddf2c
Rapan Cristian <rapancristian@yahoo.com>
2021-11-28 21:44:05 +0200

    Added a BasicTable with W3 School's styles

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
-import { useState, useEffect } from 'react';
 import './App.css';
+import { BasicTable } from './components/BasicTable';
 
 function App() {
-
-  const [data, setData] = useState([]);
-  const getData = () => {
-    fetch('data.json'
-      , {
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-        }
-      }
-    )
-      .then(function (response) {
-        console.log(response)
-        return response.json();
-      })
-      .then(function (myJson) {
-        console.log(myJson);
-        setData(myJson)
-      });
-  }
-  useEffect(()=>{
-    getData()
-  },[])
-
   return (
     <div className="App">
-       {data.map(entry => <div>{entry["Company Name"]}</div>)}
-   </div>
+      <BasicTable />
+    </div>
   );
 }
 
---
+++ src/components/columns.js
@@ -0,0 +1,51 @@
+export const COLUMNS = [
+    {
+        Header: "Company Name",
+        accessor: "Company Name"
+    },
+    {
+
+        Header: "Total Revenue",
+        accessor: "Total Revenue"
+    },
+    {
+
+        Header: "Company Market Cap",
+        accessor: "Company Market Cap"
+    },
+    {
+
+        Header: "Women Managers",
+        accessor: "Women Managers"
+    },
+    {
+
+        Header: "Women Employees",
+        accessor: "Women Employees"
+    },
+    {
+
+        Header: "ESG Score",
+        accessor: "ESG Score"
+    },
+    {
+
+        Header: "CO2 Scope 1 & 2 Adjusted",
+        accessor: "CO2 Scope 1 & 2 Adjusted"
+    },
+    {
+
+        Header: "CO2 Scope 1 & 2 Revenue Adjusted",
+        accessor: "CO2 Scope 1 & 2 Revenue Adjusted"
+    },
+    {
+
+        Header: "CO2 Scope 3 Adjusted",
+        accessor: "CO2 Scope 3 Adjusted"
+    },
+    {
+
+        Header: "CO2 Scope 3 Revenue Adjusted",
+        accessor: "CO2 Scope 3 Revenue Adjusted"
+    }
+]
