e5521429212fa3eda3c8ce6a80bf828d19791db0
Rapan Cristian <rapancristian@yahoo.com>
2021-11-28 23:17:04 +0200

    Added Pagination

--- src/components/BasicTable.js
+++ src/components/BasicTable.js
@@ -1,5 +1,5 @@
 import React, { useMemo } from 'react'
-import { useTable, useSortBy } from 'react-table'
+import { useTable, useSortBy, usePagination } from 'react-table'
 import MOCK_DATA from './MOCK_DATA.json'
 import { GROUPED_COLUMNS } from './columns'
 import './table.css'
@@ -10,11 +10,14 @@ export const BasicTable = () => {
     const tableInstance = useTable({
         columns,
         data,
-    }, useSortBy)
+    }, useSortBy, usePagination)
 
-    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = tableInstance
+    const { getTableProps, getTableBodyProps, headerGroups, page, prepareRow, nextPage, previousPage, canPreviousPage, canNextPage, state, gotoPage } = tableInstance
+
+    const { pageIndex } = state
 
     return (
+        <>
         <table {...getTableProps()}>
             <thead>
                 {headerGroups.map(headerGroup => (
@@ -33,7 +36,7 @@ export const BasicTable = () => {
                 ))}
             </thead>
             <tbody {...getTableBodyProps()}>
-                {rows.map(row => {
+                {page.map(row => {
                     prepareRow(row)
                     return (
                         <tr {...row.getRowProps()}>
@@ -45,5 +48,26 @@ export const BasicTable = () => {
                 })}
             </tbody>
         </table>
+        <div>
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          Previous
+        </button> {' '}
+        <span>
+        PAGE {' '}
+          <input
+            defaultValue={pageIndex + 1}
+            value={pageIndex + 1}
+            onChange={e => {
+              const pageNumber = e.target.value ? Number(e.target.value) - 1 : 0
+              gotoPage(pageNumber)
+            }}
+            style={{ width: '50px', border: 'none', outline: 'none', borderBottom: "1px solid #303030", textAlign: 'center' }}
+          />
+        </span>{' '}
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          Next
+        </button>
+        </div>
+        </>
     )
 }
